0a282c853c35cb0ff2876807c744ab667d01d8c2
Ileana Padilla <Ile@Ileanas-Mac.local>
2014-04-28 18:57:26 -0500

    Initial Commit

---
+++ final/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  final
+//
+//  Created by Ileana Padilla on 4/28/14.
+//  Copyright (c) 2014 paul. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ final/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  final
+//
+//  Created by Ileana Padilla on 4/28/14.
+//  Copyright (c) 2014 paul. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
